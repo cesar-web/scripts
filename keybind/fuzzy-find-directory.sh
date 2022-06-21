b3c978cbe2ff813d47d6d24a2b1093d951c9bc4e
void <void@example.com>
2022-06-21 11:12:04 -0500

    Changed script to open files with spaces

--- keybind/fuzzy-find-directory.sh
+++ keybind/fuzzy-find-directory.sh
@@ -2,13 +2,12 @@
 
 # Go to home directory and search for directories
 cd
-directory="$(find * ~/.bin -type d | amenu)"
+directory="$(fd -E .cache -E .vim -E .local -E go -H --type d | cut -c 3- | fzf --layout=reverse --info=inline --border --margin=1 --padding=1)"
 
 # Dont open anything if the user quit
 if [ "$directory" != '' ]; then
-    cd $directory
+    cd "$directory"
     pwd
-    echo " "
     ls -la --color=always --group-directories-first
     zsh
 fi
